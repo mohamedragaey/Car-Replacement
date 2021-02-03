404c67d17fd1324d184098736f5c039933aebfe2
mohamedragaey <mohamedragaey33@gmail.com>
2021-02-03 20:35:00 +0200

    Removing margin Bottom from conditions for the initiative.

--- src/components/HomePageContent/ConditionsForTheInitiative/Styles.js
+++ src/components/HomePageContent/ConditionsForTheInitiative/Styles.js
@@ -1,9 +1,7 @@
 import {makeStyles} from '@material-ui/core/styles'
 
 export const useStyles = makeStyles((theme) => ({
-  conditionsForTheInitiative: {
-    marginBottom: theme.spacing(6)
-  },
+  conditionsForTheInitiative: {},
   conditionsForTheInitiativeItemContent: {
     display: 'flex',
     flexWrap: 'wrap',
